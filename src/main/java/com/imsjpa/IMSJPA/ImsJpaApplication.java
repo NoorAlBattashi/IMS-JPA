d1253ba9b46d05a5fb6cdddf8c28dc55576ed07b
NoorAlBattashi <nooralbattashi97@gmail.com>
2023-04-06 08:51:54 +0400

    add project files in the branch

---
+++ src/main/java/com/imsjpa/IMSJPA/ImsJpaApplication.java
@@ -0,0 +1,13 @@
+package com.imsjpa.IMSJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImsJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImsJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/imsjpa/IMSJPA/ImsJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imsjpa.IMSJPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImsJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
